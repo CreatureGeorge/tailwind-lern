6f857e695e0e91088508d7c4e28f7b92fe680b26
CreatureGeorge <george@creaturenetwork.dev>
2022-09-22 19:37:28 -0400

    update styling

--- pages/qr-code/index.jsx
+++ pages/qr-code/index.jsx
@@ -2,17 +2,17 @@ import Head from "next/head";
 
 export default function QRCode() {
   return (
-    <html>
+    <div>
       <Head>
         <link rel="shortcut icon" type="image/png" sizes="32x32" href="/images/qr-code/favicon-32x32.png" />
         <title>Frontend Mentor | QR code component</title>
       </Head>
-      <body class="bg-lightgray w-screen h-screen">
+      <div class="bg-lightgray w-screen h-screen">
         <div class="flex h-screen">
-          <div class="bg-white flex flex-col m-auto p-4 rounded-2xl w-[310px]">
+          <div class="bg-white flex flex-col m-auto p-4 rounded-2xl w-[320px]">
             <img class="rounded-xl" src="/images/qr-code/image-qr-code.png" />
             <div class='font-outfit text-center p-3'>
-              <p class='text-lg text-[20.5px] font-bold text-darkblue leading-tight mt-3 mb-4'>Improve your front-end skills by building projects</p>
+              <p class='text-lg text-[21px] font-bold text-darkblue leading-tight mt-4 mb-4'>Improve your front-end skills by building projects</p>
               <p class="text-[15px] font-normal text-grayblue leading-tight mb-3">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
           </div>
@@ -21,6 +21,7 @@ export default function QRCode() {
           Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
           Coded by <a href="#" class=''>Goerge Mueller</a>.
         </div>
-      </body>
-    </html>)
+      </div>
+    </div>
+)
 }
