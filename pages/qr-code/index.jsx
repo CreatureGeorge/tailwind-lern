c7ade22f4662a083fea4db8e9782477d0eacc051
CreatureGeorge <george@creaturenetwork.dev>
2022-09-22 19:39:15 -0400

    alt code update

--- pages/qr-code/index.jsx
+++ pages/qr-code/index.jsx
@@ -10,7 +10,7 @@ export default function QRCode() {
       <div class="bg-lightgray w-screen h-screen">
         <div class="flex h-screen">
           <div class="bg-white flex flex-col m-auto p-4 rounded-2xl w-[320px]">
-            <img class="rounded-xl" src="/images/qr-code/image-qr-code.png" />
+            <img class="rounded-xl" alt="QR Code to Frontend Mentor" src="/images/qr-code/image-qr-code.png" />
             <div class='font-outfit text-center p-3'>
               <p class='text-lg text-[21px] font-bold text-darkblue leading-tight mt-4 mb-4'>Improve your front-end skills by building projects</p>
               <p class="text-[15px] font-normal text-grayblue leading-tight mb-3">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
