b35c1c7a874e59c227a17ed9622417e636bf9a17
CreatureGeorge <george@creaturenetwork.dev>
2022-09-22 19:23:44 -0400

    front-page update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const people = [
@@ -23,17 +24,16 @@ const people = [
 
 export default function Home() {
   return (
-    <div class="grid grid-cols-1 md:grid-cols-2">
-      <div class="bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-        <div>
-          <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
-            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
-          </span>
+    <div class="w-screen h-screen">
+      <div class="flex h-screen">
+        <div class="flex flex-col m-auto text-center">
+        <h3 class="text-slate-900 text-base font-medium">Design Challenges:</h3>
+        <div class="flex flex-col underline text-blue-600 hover:text-blue-800 mt-5 text-sm">
+          <Link  href="/qr-code">
+            qr-code
+          </Link>
         </div>
-        <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
-        <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-          The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-        </p>
+        </div>
       </div>
     </div>
   )
