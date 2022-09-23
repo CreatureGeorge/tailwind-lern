a35292247efe3e9e5b525903d558b4be6c1a2893
CreatureGeorge <george@creaturenetwork.dev>
2022-09-22 20:24:21 -0400

    landmarks added

--- pages/index.js
+++ pages/index.js
@@ -3,38 +3,25 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
-const people = [
-  {
-    name: "George Mueller",
-    title: "Dev Boi",
-    email: "george@lifenetwork.dev",
-    imageUrl: "/img/george.png"
-  }, {
-    name: "George Mueller",
-    title: "Dev Boi",
-    email: "george@lifenetwork.dev",
-    imageUrl: "/img/george.png"
-  }, {
-    name: "George Mueller",
-    title: "Dev Boi",
-    email: "george@lifenetwork.dev",
-    imageUrl: "/img/george.png"
-  },
-]
-
 export default function Home() {
   return (
-    <div class="w-screen h-screen">
-      <div class="flex h-screen">
-        <div class="flex flex-col m-auto text-center">
-        <h3 class="text-slate-900 text-base font-medium">Design Challenges:</h3>
-        <div class="flex flex-col underline text-blue-600 hover:text-blue-800 mt-5 text-sm">
-          <Link  href="/qr-code">
-            qr-code
-          </Link>
-        </div>
-        </div>
-      </div>
+    <div>
+      <Head>
+        <link rel="shortcut icon" type="image/png" sizes="32x32" href="/images/qr-code/favicon-32x32.png" />
+        <title>Frontend Mentor | Tailwind Home</title>
+      </Head>
+      <body class="w-screen h-screen">
+        <main class="flex h-screen">
+          <div class="flex flex-col m-auto text-center">
+            <h3 class="text-slate-900 text-base font-medium">Design Challenges:</h3>
+            <div class="flex flex-col underline text-blue-600 hover:text-blue-800 mt-5 text-sm">
+              <Link href="/qr-code">
+                qr-code
+              </Link>
+            </div>
+          </div>
+        </main>
+      </body>
     </div>
   )
 }
